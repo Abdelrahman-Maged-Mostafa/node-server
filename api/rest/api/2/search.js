91efdc7f5a52790d89016411a306008a27539c8e
Abdelrahman-Maged-Mostafa <podapoda_poda@yahoo.com>
2026-01-14 15:15:11 +0200

    Update issue handler with new API URLs

--- api/rest/api/2/search.js
+++ api/rest/api/2/search.js
@@ -3,27 +3,27 @@ export default function handler(req, res) {
     {
       expand: "renderedFields,names,schema,operations,editmeta,changelog,versionedRepresentations",
       id: "10113",
-      self: "https://podapodapoda.atlassian.net/rest/api/3/issue/10113",
+      self: "https://node-server-rose.vercel.app/api/rest/api/3/issue/10113",
       key: "CPG-15",
       fields: {
         summary: "new one",
         created: "2026-01-12T14:49:17.618+0200",
         project: {
-          self: "https://podapodapoda.atlassian.net/rest/api/3/project/10000",
+          self: "https://node-server-rose.vercel.app/api/rest/api/3/project/10000",
           id: "10000",
           key: "CPG",
           name: "Cryptocurrency Payment Gateway",
           projectTypeKey: "software",
           simplified: true,
           avatarUrls: {
-            "48x48": "https://podapodapoda.atlassian.net/rest/api/3/universal_avatar/view/type/project/avatar/10411",
-            "24x24": "https://podapodapoda.atlassian.net/rest/api/3/universal_avatar/view/type/project/avatar/10411?size=small",
-            "16x16": "https://podapodapoda.atlassian.net/rest/api/3/universal_avatar/view/type/project/avatar/10411?size=xsmall",
-            "32x32": "https://podapodapoda.atlassian.net/rest/api/3/universal_avatar/view/type/project/avatar/10411?size=medium"
+            "48x48": "https://node-server-rose.vercel.app/api/rest/api/3/universal_avatar/view/type/project/avatar/10411",
+            "24x24": "https://node-server-rose.vercel.app/api/rest/api/3/universal_avatar/view/type/project/avatar/10411?size=small",
+            "16x16": "https://node-server-rose.vercel.app/api/rest/api/3/universal_avatar/view/type/project/avatar/10411?size=xsmall",
+            "32x32": "https://node-server-rose.vercel.app/api/rest/api/3/universal_avatar/view/type/project/avatar/10411?size=medium"
           }
         },
         reporter: {
-          self: "https://podapodapoda.atlassian.net/rest/api/3/user?accountId=712020%3Af0f33540-9622-40d9-a653-866ec4bbeea8",
+          self: "https://node-server-rose.vercel.app/api/rest/api/3/user?accountId=712020%3Af0f33540-9622-40d9-a653-866ec4bbeea8",
           accountId: "712020:f0f33540-9622-40d9-a653-866ec4bbeea8",
           emailAddress: "podapoda_poda@yahoo.com",
           avatarUrls: {
@@ -39,20 +39,20 @@ export default function handler(req, res) {
         },
         assignee: null,
         priority: {
-          self: "https://podapodapoda.atlassian.net/rest/api/3/priority/3",
-          iconUrl: "https://podapodapoda.atlassian.net/images/icons/priorities/medium_new.svg",
+          self: "https://node-server-rose.vercel.app/api/rest/api/3/priority/3",
+          iconUrl: "https://node-server-rose.vercel.app/api/images/icons/priorities/medium_new.svg",
           name: "Medium",
           id: "3"
         },
         updated: "2026-01-13T10:10:34.596+0200",
         status: {
-          self: "https://podapodapoda.atlassian.net/rest/api/3/status/10000",
+          self: "https://node-server-rose.vercel.app/api/rest/api/3/status/10000",
           description: "",
-          iconUrl: "https://podapodapoda.atlassian.net/images/icons/statuses/generic.png",
+          iconUrl: "https://node-server-rose.vercel.app/api/images/icons/statuses/generic.png",
           name: "To Do",
           id: "10000",
           statusCategory: {
-            self: "https://podapodapoda.atlassian.net/rest/api/3/statuscategory/2",
+            self: "https://node-server-rose.vercel.app/api/rest/api/3/statuscategory/2",
             id: 2,
             key: "new",
             colorName: "blue-gray",
@@ -67,4 +67,5 @@ export default function handler(req, res) {
 
   res.status(200).json({ issues,isLast: true });
 }
+
 
