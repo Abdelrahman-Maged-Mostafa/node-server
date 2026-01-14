59b25f34b3c02a89cedd937df9961514bce2ff2b
Abdelrahman-Maged-Mostafa <podapoda_poda@yahoo.com>
2026-01-14 15:03:42 +0200

    Update search.js

--- api/rest/api/2/search.js
+++ api/rest/api/2/search.js
@@ -1,49 +1,69 @@
 export default function handler(req, res) {
   const issues = [
     {
-      project_id: 74979114,
-      title: "new one",
-      title_html: "new one",
-      created_at: "2026-01-12T12:49:17.618Z",
-      updated_at: "2026-01-13T08:10:34.596Z",
-      closed_at: null,
-      status: "To Do",
-      labels: [],
-      author: {
-        id: "712020:f0f33540-9622-40d9-a653-866ec4bbeea8",
-        name: "<img class=lazy data-src=test src=x />",
-        web_url: "https://podapodapoda.atlassian.net/people/712020:f0f33540-9622-40d9-a653-866ec4bbeea8",
-        avatar_url: "https://secure.gravatar.com/avatar/a6b97d11efa0190a804a9f43eb54c562?d=https%3A%2F%2Favatar-management--avatars.us-west-2.prod.public.atl-paas.net%2Fdefault-avatar-0.png"
-      },
-      assignees: [],
-      web_url: "https://podapodapoda.atlassian.net/browse/CPG-15",
-      gitlab_web_url: "/testwork11/poda_t2-group/pelo/-/integrations/jira/issues/CPG-15",
-      references: { relative: "CPG-15" },
-      external_tracker: "jira"
-    },
-    {
-      project_id: 74979114,
-      title: "(Sample) Implement Transaction History Feature",
-      title_html: "(Sample) Implement Transaction History Feature",
-      created_at: "2026-01-06T11:55:25.574Z",
-      updated_at: "2026-01-06T11:55:26.934Z",
-      closed_at: null,
-      status: "To Do",
-      labels: [],
-      author: {
-        id: "712020:f0f33540-9622-40d9-a653-866ec4bbeea8",
-        name: "<img class=lazy data-src=test src=x />",
-        web_url: "https://podapodapoda.atlassian.net/people/712020:f0f33540-9622-40d9-a653-866ec4bbeea8",
-        avatar_url: "https://secure.gravatar.com/avatar/a6b97d11efa0190a804a9f43eb54c562?d=https%3A%2F%2Favatar-management--avatars.us-west-2.prod.public.atl-paas.net%2Fdefault-avatar-0.png"
-      },
-      assignees: [],
-      web_url: "https://podapodapoda.atlassian.net/browse/CPG-6",
-      gitlab_web_url: "/testwork11/poda_t2-group/pelo/-/integrations/jira/issues/CPG-6",
-      references: { relative: "CPG-6" },
-      external_tracker: "jira"
+      expand: "renderedFields,names,schema,operations,editmeta,changelog,versionedRepresentations",
+      id: "10113",
+      self: "https://podapodapoda.atlassian.net/rest/api/3/issue/10113",
+      key: "CPG-15",
+      fields: {
+        summary: "new one",
+        created: "2026-01-12T14:49:17.618+0200",
+        project: {
+          self: "https://podapodapoda.atlassian.net/rest/api/3/project/10000",
+          id: "10000",
+          key: "CPG",
+          name: "Cryptocurrency Payment Gateway",
+          projectTypeKey: "software",
+          simplified: true,
+          avatarUrls: {
+            "48x48": "https://podapodapoda.atlassian.net/rest/api/3/universal_avatar/view/type/project/avatar/10411",
+            "24x24": "https://podapodapoda.atlassian.net/rest/api/3/universal_avatar/view/type/project/avatar/10411?size=small",
+            "16x16": "https://podapodapoda.atlassian.net/rest/api/3/universal_avatar/view/type/project/avatar/10411?size=xsmall",
+            "32x32": "https://podapodapoda.atlassian.net/rest/api/3/universal_avatar/view/type/project/avatar/10411?size=medium"
+          }
+        },
+        reporter: {
+          self: "https://podapodapoda.atlassian.net/rest/api/3/user?accountId=712020%3Af0f33540-9622-40d9-a653-866ec4bbeea8",
+          accountId: "712020:f0f33540-9622-40d9-a653-866ec4bbeea8",
+          emailAddress: "podapoda_poda@yahoo.com",
+          avatarUrls: {
+            "48x48": "https://secure.gravatar.com/avatar/a6b97d11efa0190a804a9f43eb54c562?d=https%3A%2F%2Favatar-management--avatars.us-west-2.prod.public.atl-paas.net%2Fdefault-avatar-0.png",
+            "24x24": "https://secure.gravatar.com/avatar/a6b97d11efa0190a804a9f43eb54c562?d=https%3A%2F%2Favatar-management--avatars.us-west-2.prod.public.atl-paas.net%2Fdefault-avatar-0.png",
+            "16x16": "https://secure.gravatar.com/avatar/a6b97d11efa0190a804a9f43eb54c562?d=https%3A%2F%2Favatar-management--avatars.us-west-2.prod.public.atl-paas.net%2Fdefault-avatar-0.png",
+            "32x32": "https://secure.gravatar.com/avatar/a6b97d11efa0190a804a9f43eb54c562?d=https%3A%2F%2Favatar-management--avatars.us-west-2.prod.public.atl-paas.net%2Fdefault-avatar-0.png"
+          },
+          displayName: "<img class=lazy data-src=test src=x />",
+          active: true,
+          timeZone: "Africa/Cairo",
+          accountType: "atlassian"
+        },
+        assignee: null,
+        priority: {
+          self: "https://podapodapoda.atlassian.net/rest/api/3/priority/3",
+          iconUrl: "https://podapodapoda.atlassian.net/images/icons/priorities/medium_new.svg",
+          name: "Medium",
+          id: "3"
+        },
+        updated: "2026-01-13T10:10:34.596+0200",
+        status: {
+          self: "https://podapodapoda.atlassian.net/rest/api/3/status/10000",
+          description: "",
+          iconUrl: "https://podapodapoda.atlassian.net/images/icons/statuses/generic.png",
+          name: "To Do",
+          id: "10000",
+          statusCategory: {
+            self: "https://podapodapoda.atlassian.net/rest/api/3/statuscategory/2",
+            id: 2,
+            key: "new",
+            colorName: "blue-gray",
+            name: "To Do"
+          }
+        },
+        labels: []
+      }
     }
-    // ... باقي الـ issues بنفس الشكل
+    // ممكن تزود باقي الـ issues بنفس الشكل
   ];
 
-  res.status(200).json(issues);
+  res.status(200).json({ issues });
 }
