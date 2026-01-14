da5cbfffc8900a08693ed54140b7eba66fa098c5
Abdelrahman-Maged-Mostafa <podapoda_poda@yahoo.com>
2026-01-14 16:02:22 +0200

    Update search.js

--- api/rest/api/2/search.js
+++ api/rest/api/2/search.js
@@ -1,13 +1,70 @@
 export default function handler(req, res) {
-  const errorMessages = [
-    "The requested API has been removed. Please migrate to the /rest/api/3/search/jql API. A full migration guideline is available at https://developer.atlassian.com/changelog/#CHANGE-2046"
+  const issues = [
+    {
+      expand: "renderedFields,names,schema,operations,editmeta,changelog,versionedRepresentations",
+      id: "10113",
+      self: "https://podapodapoda.atlassian.net/rest/api/3/issue/10113",
+      key: "CPG-15",
+      fields: {
+        summary: "<img sec=x />",
+        created: "2026-01-12T14:49:17.618+0200",
+        resolutiondate: null,
+        project: {
+          self: "https://podapodapoda.atlassian.net/rest/api/3/project/10000",
+          id: "10000",
+          key: "CPG",
+          name: "Cryptocurrency Payment Gateway",
+          projectTypeKey: "software",
+          simplified: true,
+          avatarUrls: {
+            "48x48": "https://podapodapoda.atlassian.net/rest/api/3/universal_avatar/view/type/project/avatar/10411",
+            "24x24": "https://podapodapoda.atlassian.net/rest/api/3/universal_avatar/view/type/project/avatar/10411?size=small",
+            "16x16": "https://podapodapoda.atlassian.net/rest/api/3/universal_avatar/view/type/project/avatar/10411?size=xsmall",
+            "32x32": "https://podapodapoda.atlassian.net/rest/api/3/universal_avatar/view/type/project/avatar/10411?size=medium"
+          }
+        },
+        reporter: {
+          self: "https://podapodapoda.atlassian.net/rest/api/3/user?accountId=712020%3Af0f33540-9622-40d9-a653-866ec4bbeea8",
+          accountId: "712020:f0f33540-9622-40d9-a653-866ec4bbeea8",
+          emailAddress: "podapoda_poda@yahoo.com",
+          avatarUrls: {
+            "48x48": "https://secure.gravatar.com/avatar/a6b97d11efa0190a804a9f43eb54c562?d=https://avatar-management--avatars.us-west-2.prod.public.atl-paas.net/default-avatar-0.png",
+            "24x24": "https://secure.gravatar.com/avatar/a6b97d11efa0190a804a9f43eb54c562?d=https://avatar-management--avatars.us-west-2.prod.public.atl-paas.net/default-avatar-0.png",
+            "16x16": "https://secure.gravatar.com/avatar/a6b97d11efa0190a804a9f43eb54c562?d=https://avatar-management--avatars.us-west-2.prod.public.atl-paas.net/default-avatar-0.png",
+            "32x32": "https://secure.gravatar.com/avatar/a6b97d11efa0190a804a9f43eb54c562?d=https://avatar-management--avatars.us-west-2.prod.public.atl-paas.net/default-avatar-0.png"
+          },
+          displayName: "<img class=lazy data-src=test src=x />",
+          active: true,
+          timeZone: "Africa/Cairo",
+          accountType: "atlassian"
+        },
+        assignee: null,
+        priority: {
+          self: "https://podapodapoda.atlassian.net/rest/api/3/priority/3",
+          iconUrl: "https://podapodapoda.atlassian.net/images/icons/priorities/medium_new.svg",
+          name: "Medium",
+          id: "3"
+        },
+        updated: "2026-01-14T15:07:19.530+0200",
+        status: {
+          self: "https://podapodapoda.atlassian.net/rest/api/3/status/10000",
+          description: "",
+          iconUrl: "https://podapodapoda.atlassian.net/images/icons/statuses/generic.png",
+          name: "To Do",
+          id: "10000",
+          statusCategory: {
+            self: "https://podapodapoda.atlassian.net/rest/api/3/statuscategory/2",
+            id: 2,
+            key: "new",
+            colorName: "blue-gray",
+            name: "To Do"
+          }
+        },
+        labels: []
+      }
+    },
+    // ممكن تزود باقي الـ issues بنفس الشكل اللي وريتهولي
   ];
- const errors = {
 
-  };
-  res.status(200).json({ errorMessages,errors });
+  res.status(200).json({ issues });
 }
-
-
-
-
